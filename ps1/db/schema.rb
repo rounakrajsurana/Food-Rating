004981a064a6144f110701a1f496e554b70b2d77
st120546@ait.ac.th <st120546@ait.ac.th>
2018-09-24 21:30:49 +0700

    Master Branch Commit working

--- ps1/db/schema.rb
+++ ps1/db/schema.rb
@@ -21,6 +21,38 @@ ActiveRecord::Schema.define(version: 2018_09_10_081221) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "my_stocks", id: false, force: :cascade do |t|
+    t.string "symbol", limit: 20, null: false
+    t.integer "n_shares", null: false
+    t.date "date_acquired", null: false
+  end
+
+  create_table "newly_acquired_stocks", id: false, force: :cascade do |t|
+    t.string "symbol", limit: 20, null: false
+    t.integer "n_shares", null: false
+    t.date "date_acquired", null: false
+  end
+
+  create_table "problems", force: :cascade do |t|
+    t.integer "pid"
+    t.bigint "problemset_id"
+    t.string "pname"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["problemset_id"], name: "index_problems_on_problemset_id"
+  end
+
+  create_table "problemsets", force: :cascade do |t|
+    t.integer "psid"
+    t.string "psname"
+    t.date "date_assign"
+    t.date "date_due"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "quotations", force: :cascade do |t|
     t.string "author_name"
     t.string "category"
@@ -29,4 +61,22 @@ ActiveRecord::Schema.define(version: 2018_09_10_081221) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "solutions", force: :cascade do |t|
+    t.integer "sid"
+    t.bigint "problem_id"
+    t.text "desc"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["problem_id"], name: "index_solutions_on_problem_id"
+  end
+
+  create_table "stock_prices", id: false, force: :cascade do |t|
+    t.string "symbol", limit: 20, null: false
+    t.date "quote_date", null: false
+    t.integer "price", null: false
+  end
+
+  add_foreign_key "problems", "problemsets"
+  add_foreign_key "solutions", "problems"
 end
