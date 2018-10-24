90252d6e6b35296a765e7508c9dd756eadd87003
rounakrajsurana <st119979@ait.asia>
2018-10-25 00:57:04 +0700

    adding of active column in user table

--- ps/db/schema.rb
+++ ps/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_035247) do
+ActiveRecord::Schema.define(version: 2018_10_24_174401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_035247) do
     t.string "contactno"
     t.boolean "admin"
     t.boolean "stall"
+    t.boolean "active"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
