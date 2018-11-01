9e14abb3beea10d6c11c9a8c514f672dd897da89
rounakrajsurana <st119979@ait.asia>
2018-11-01 10:20:44 +0700

    adding bootstrap

--- ps/app/models/ability.rb
+++ ps/app/models/ability.rb
@@ -4,27 +4,29 @@ class Ability
     user ||= User.new
     can :read, :all
     # return unless user && user.admin?
-      if user
+  
+        if user.active
           can :read, :all
-          if user.admin?
-            can :manage, :all
-            can :dashboard, :all
-            can :access, :rails_admin       # only allow admin users to access Rails Admin
-            # can :read, :dashboard           # allow access to dashboard
-          elsif user.stall?
-            # can :read, :all
-            can :create, Dish
-            can [:update, :destroy], Dish do |d|
-              d.user == user
-            end
-          elsif user
-            can :create, Rating
-            can [:update, :destroy], Rating do |r|
-              r.user == user
-            end
+            if user.admin?
+              can :manage, :all
+              can :dashboard, :all
+              can :access, :rails_admin       # only allow admin users to access Rails Admin
+              # can :read, :dashboard           # allow access to dashboard
+            elsif user.stall?
+              # can :read, :all
+              can :create, Dish
+              can [:update, :destroy], Dish do |d|
+                d.user == user
+              end
+            elsif user
+              can :create, Rating
+              can [:update, :destroy], Rating do |r|
+                r.user == user
+              end
     # else
     #    can :read, :all
-          end
+          
+        end 
       end
   end
 end
