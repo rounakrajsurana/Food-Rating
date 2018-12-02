ed78e43aef86dc27b58ca482847b189d29ff1a5a
rounakrajsurana <st119979@ait.asia>
2018-12-02 22:15:55 +0700

    continuation of the previous commit to upload images of stall and dishes

--- ps/app/models/dish.rb
+++ ps/app/models/dish.rb
@@ -3,11 +3,11 @@ class Dish < ApplicationRecord
   belongs_to :stall
   belongs_to :user
   has_many :ratings, dependent: :destroy
-  has_many :log_ratings, dependent: :destroy
 
   validates_presence_of :name
   validates_presence_of :desc
   validates_presence_of :picture
   validates_presence_of :category_id
   validates_presence_of :stall_id
+  has_one_attached :picture
 end
